c611ea42f9579b42429d6220f2a7e1128694046f
tpabhijith <itsabhi.tp@gmail.com>
2021-10-04 16:48:01 +0530

    title added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Home from "./components/screens/Home"
 import Header from './components/screens/Header';
 import Place from './components/screens/Place';
+import Helmet from "react-helmet";
 
 function App() {
   return (
     <Router>
       <>
+        <Helmet>
+          <title>Places | Travel Guide</title>
+        </Helmet>
         <Header />
         <Switch>
           <Route path="/" exact component={Home} />
